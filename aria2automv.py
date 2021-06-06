eef279905e1943af63a69009e8a8a7bd46b59646
dayatxd69 <83796406+dayatxd69@users.noreply.github.com>
2021-06-06 13:52:12 +0800

    Create aria2automv.py

---
+++ aria2automv.py
@@ -0,0 +1,17 @@
+import os, re, time, shutil, glob
+
+destination = "C:/Users/runneradmin/Documents/GoogleDrive/aria2"
+
+while True:
+  rootdir = "C:/Users/runneradmin/Downloads/aria2/"
+  regex = re.compile('(.*zip$)|(.*rar$)|(.*mp4$)|(.*mkv$)')
+  if not glob.glob(rootdir+"*.aria2"):
+    for root, dirs, files in os.walk(rootdir):
+      for file in files:
+        if regex.match(file):
+          shutil.move(root+file, destination)
+          print(root+file, destination)
+        else: pass
+  else: pass
+  time.sleep(30)
+  pass
